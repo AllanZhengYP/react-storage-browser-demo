d8cab45a43ebb2400524cbd6ac676184ba11d52f
Allan Zheng <zheallan@amazon.com>
2024-08-07 12:50:01 -0700

    trigger credentials provider error

--- src/utils/s3ManagedAuthAdapter.ts
+++ src/utils/s3ManagedAuthAdapter.ts
@@ -17,6 +17,7 @@ const oidcClient = generateClient<Schema>();
 
 const fetchBaseCredentials = async (): Promise<CredentialsProviderOutput> => {
   const { __raw } = await Auth.getIdToken();
+  throw new Error('fake error');
   const { data, errors } = await oidcClient.queries.oidc({
     idToken: __raw
   }, { authMode: 'apiKey' });
